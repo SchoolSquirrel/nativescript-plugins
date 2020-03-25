0c53e50123a16014a68aa054921600dbc149ac35
H. Rüger <hannesrueger@gmx.de>
2020-03-25 11:34:07 -1000

    added dummy ios structure

--- src/emoji-picker.ios.ts
+++ src/emoji-picker.ios.ts
@@ -1,5 +1,28 @@
-import { EmojiPickerBase } from "./emoji-picker.common";
+import { CSSType } from "tns-core-modules/ui/text-base";
+import { EmojiLabel as EmojiLabelDefinition } from ".";
+import { EmojiPicker as EmojiPickerDefinition } from ".";
+import { EmojiLabelBase, EmojiPickerBase } from "./emoji-picker.common";
+export * from "tns-core-modules/ui/text-base";
+export * from "tns-core-modules/ui/text-field/text-field";
 
-export class MyButton extends EmojiPickerBase {
+@CSSType("EmojiPicker")
+export class EmojiPicker extends EmojiPickerBase implements EmojiPickerDefinition {
+    public initNativeView(): void {
+        //
+    }
 
+    public disposeNativeView(): void {
+        //
+    }
+}
+
+@CSSType("EmojiLabel")
+export class EmojiLabel extends EmojiLabelBase implements EmojiLabelDefinition {
+    public initNativeView(): void {
+        //
+    }
+
+    public disposeNativeView(): void {
+        //
+    }
 }
