1e9f369aa10476767ac118532b35050f4a9275ae
Hannes Rüger <hannesrueger@gmx.de>
2020-09-15 18:30:06 +0200

    added Angular support for emoji picker

---
+++ packages/emoji-picker/angular/index.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
+import { EmojiPicker } from '@schoolsquirrel/emoji-picker';
+
+@NgModule()
+export class NativeScriptEmojiPickerModule {}
+
+// Uncomment this line if the package provides a custom view component
+// registerElement('EmojiPicker', () => EmojiPicker);
