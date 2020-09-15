df48bd2b640a1f55cc7b21eadc672728be34a566
Hannes Rüger <hannesrueger@gmx.de>
2020-09-15 20:05:30 +0200

    fixed angular navigation

--- apps/demo-angular/src/home.component.ts
+++ apps/demo-angular/src/home.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'demo-home',
 	templateUrl: 'home.component.html',
 })
 export class HomeComponent {
+	constructor(private router: Router) {}
 	demos = [
-		{
-			name: 'emoji-picker',
-		},
+	{
+		name: 'emoji-picker'
+	}
 	];
+	n(i) {
+		this.router.navigate(["/" + this.demos[i.index].name]);
+	}
 }
