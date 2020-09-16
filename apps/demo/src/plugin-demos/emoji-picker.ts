0bd3a3f7cfd7be35e22538aca6b7022b5d92d2dd
Hannes Rüger <hannesrueger@gmx.de>
2020-09-16 20:01:50 +0200

    added vanilla demo

--- apps/demo/src/plugin-demos/emoji-picker.ts
+++ apps/demo/src/plugin-demos/emoji-picker.ts
@@ -1,10 +1,21 @@
-import { Observable, EventData, Page } from '@nativescript/core';
+import { Observable, EventData, Page, Frame } from '@nativescript/core';
 import { DemoSharedEmojiPicker } from '@demo/shared';
-import {} from '@schoolsquirrel/emoji-picker';
+import { EmojiLabel, EmojiPicker } from '@schoolsquirrel/emoji-picker';
 
 export function navigatingTo(args: EventData) {
 	const page = <Page>args.object;
 	page.bindingContext = new DemoModel();
 }
 
-export class DemoModel extends DemoSharedEmojiPicker {}
+export class DemoModel extends DemoSharedEmojiPicker {
+	public onButtonTap(args): void {
+		const page = Frame.topmost().currentPage;
+		(page.getViewById('myEmojiPicker') as EmojiPicker).togglePopup();
+	}
+
+	public onCopyBtnTap(args): void {
+		const page = Frame.topmost().currentPage;
+		(page.getViewById('myEmojiLabel') as EmojiLabel).text = (page.getViewById('myEmojiPicker') as EmojiPicker).text;
+		return;
+	}
+}
