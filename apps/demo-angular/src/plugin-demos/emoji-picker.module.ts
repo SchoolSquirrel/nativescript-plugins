28ab10f2ed7f3f6a90ab5912571ade8f43a7b943
Hannes Rüger <hannesrueger@gmx.de>
2020-09-19 12:22:08 +0200

    fixed angular demo

--- apps/demo-angular/src/plugin-demos/emoji-picker.module.ts
+++ apps/demo-angular/src/plugin-demos/emoji-picker.module.ts
@@ -1,9 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
 import { EmojiPickerComponent } from './emoji-picker.component';
+import { EmojiPickerModule as EmojiModule } from "@schoolsquirrel/emoji-picker/angular";
 
 @NgModule({
-	imports: [NativeScriptCommonModule, NativeScriptRouterModule.forChild([{ path: '', component: EmojiPickerComponent }])],
+	imports: [NativeScriptCommonModule, NativeScriptRouterModule.forChild([{ path: '', component: EmojiPickerComponent }]), EmojiModule],
 	declarations: [EmojiPickerComponent],
 	schemas: [NO_ERRORS_SCHEMA],
 })
--- packages/emoji-picker/angular/index.ts
+++ packages/emoji-picker/angular/index.ts
@@ -3,7 +3,7 @@ import { registerElement } from '@nativescript/angular';
 import { EmojiPicker, EmojiLabel } from '@schoolsquirrel/emoji-picker';
 
 @NgModule()
-export class NativeScriptEmojiPickerModule {}
+export class EmojiPickerModule {}
 
 // Uncomment this line if the package provides a custom view component
 registerElement('EmojiPicker', () => EmojiPicker);
