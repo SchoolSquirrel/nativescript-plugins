3fe3137a5c98ad0ce672d9a18ada07e6b683023d
Hannes Rüger <hannesrueger@gmx.de>
2020-10-18 13:31:02 +0200

    (letter-avatar) added angular support

---
+++ packages/letter-avatar/angular/index.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { registerElement } from '@nativescript/angular';
+import { LetterAvatar } from '@schoolsquirrel/letter-avatar';
+
+@NgModule()
+export class NativeScriptLetterAvatarModule {}
+
+// Uncomment this line if the package provides a custom view component
+// registerElement('LetterAvatar', () => LetterAvatar);
