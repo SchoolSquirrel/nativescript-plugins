b37f5e3de38b23745c5c584f2bda75df846971f1
Hannes Rüger <hannesrueger@gmx.de>
2020-10-18 13:30:42 +0200

    (chat-ui) fixed build

--- packages/nativescript-squirrel-chat-ui/angular/index.ts
+++ packages/nativescript-squirrel-chat-ui/angular/index.ts
@@ -1,11 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativescriptSquirrelChatUiComponent } from './nativescript-squirrel-chat-ui.component';
+import { NativeScriptCommonModule } from "@nativescript/angular";
 
 @NgModule({
     declarations: [
         NativescriptSquirrelChatUiComponent,
     ],
-    imports: [],
+    imports: [
+        NativeScriptCommonModule,
+    ],
     schemas: [
         NO_ERRORS_SCHEMA,
         CUSTOM_ELEMENTS_SCHEMA,
